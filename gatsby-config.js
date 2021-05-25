6d7ed05ab364e9df9c52ba393d92b406ce3ff4af
Waterpile <larryc506@gmail.com>
2021-05-25 16:23:27 -0400

    remove ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,14 +29,6 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    "gatsby-plugin-gatsby-cloud",
-    // Add your Google Analytics ID to the .env file to enable
-    // Otherwise, this plugin can be removed
-    process.env.GOOGLE_ANALYTICS_ID && {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
-    },
+    "gatsby-plugin-gatsby-cloud"
   ].filter(Boolean),
 }
